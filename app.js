a10bf65bc7f9c5d16235763e39d4f857f0ffa8e7
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-09-25 16:05:58 +0300

    update mongodb driver to 3.1

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ passport.serializeUser(function (user, cb) {
 });
 
 passport.deserializeUser(function (username, cb) {
-  mongoClient.connect(mongodbUrl + "/auth", function (err, db) {
-    db.collection("users").findOne({ username: username }, function (err, user) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db("auth").collection("users").findOne({ username: username }, function (err, user) {
       if (err) return cb(err)
       if (!user) { return cb(null, false); }
       return cb(null, user);
-      db.close();
+      client.close();
     });
   });
 });
@@ -83,33 +83,33 @@ app.get('/mongodb', connectEnsureLogin.ensureLoggedIn({ redirectTo: "/login?sour
 // API
 //==================================================================================================
 app.get("/mongodb/api", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
-  mongoClient.connect(mongodbUrl + "/test", function (err, db) {
-    var adminDb = db.admin();
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    var adminDb = client.db("test").admin();
     adminDb.listDatabases(function (err, dbs) {
       if (req.user.username == "admin" || req.user.username == "hsyn") res.send(dbs.databases)
       else res.send([])
-      db.close()
+      client.close()
     })
   })
 })
 
 app.get("/mongodb/api/:db", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
-  mongoClient.connect(mongodbUrl + "/" + req.params.db, function (err, db) {
-    db.listCollections().toArray(function (err, items) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db(req.params.db).listCollections().toArray(function (err, items) {
       if (req.user.username == "admin" || req.user.username == "hsyn") res.send(items)
       else res.send([])
-      db.close()
+      client.close()
     })
   })
 })
 
 app.get("/mongodb/api/:db/:col", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
   req.query.users = req.user.username
-  mongoClient.connect(mongodbUrl + "/" + req.params.db, function (err, db) {
-    db.collection(req.params.col).find(req.query).toArray(function (err, docs) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db(req.params.db).collection(req.params.col).find(req.query).toArray(function (err, docs) {
       if (err) res.send({ error: err })
       else res.send(docs)
-      db.close();
+      client.close();
     });
   });
 })
@@ -121,11 +121,11 @@ app.post("/mongodb/api/:db/:col", connectEnsureLogin.ensureLoggedIn(), function 
   else { req.body.owners = [req.user.username] }
   if (req.body.users.indexOf("admin") === -1) { req.body.users.push("admin") }
   if (req.body.owners.indexOf("admin") === -1) { req.body.owners.push("admin") }
-  mongoClient.connect(mongodbUrl + "/" + req.params.db, function (err, db) {
-    db.collection(req.params.col).insertOne(req.body, function (err, r) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db(req.params.db).collection(req.params.col).insertOne(req.body, function (err, r) {
       if (err) res.send({ error: err })
       else res.send(Object.assign({}, r.result, { insertedId: r.insertedId }))
-      db.close()
+      client.close()
     })
   });
 })
@@ -133,11 +133,11 @@ app.post("/mongodb/api/:db/:col", connectEnsureLogin.ensureLoggedIn(), function 
 app.get("/mongodb/api/:db/:col/:id", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
   var query = { users: req.user.username }
   query._id = mongoObjectId(req.params.id)
-  mongoClient.connect(mongodbUrl + "/" + req.params.db, function (err, db) {
-    db.collection(req.params.col).findOne(query, function (err, doc) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db(req.params.db).collection(req.params.col).findOne(query, function (err, doc) {
       if (err) res.send({ error: err })
       else res.send(doc)
-      db.close();
+      client.close();
     });
   });
 })
@@ -146,11 +146,11 @@ app.put("/mongodb/api/:db/:col/:id", connectEnsureLogin.ensureLoggedIn(), functi
   var query = { owners: req.user.username }
   query._id = mongoObjectId(req.params.id)
   delete req.body._id
-  mongoClient.connect(mongodbUrl + "/" + req.params.db, function (err, db) {
-    db.collection(req.params.col).updateOne(query, { "$set": req.body }, function (err, r) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db(req.params.db).collection(req.params.col).updateOne(query, { "$set": req.body }, function (err, r) {
       if (err) res.send({ error: err })
       else res.send(Object.assign({}, r.result))
-      db.close()
+      client.close()
     })
   })
 })
@@ -158,11 +158,11 @@ app.put("/mongodb/api/:db/:col/:id", connectEnsureLogin.ensureLoggedIn(), functi
 app.delete("/mongodb/api/:db/:col/:id", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
   var query = { owners: req.user.username }
   query._id = mongoObjectId(req.params.id)
-  mongoClient.connect(mongodbUrl + "/" + req.params.db, function (err, db) {
-    db.collection(req.params.col).deleteOne(query, function (err, r) {
+  mongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
+    client.db(req.params.db).collection(req.params.col).deleteOne(query, function (err, r) {
       if (err) res.send({ error: err })
       else res.send(Object.assign({}, r.result))
-      db.close()
+      client.close()
     })
   })
 })
